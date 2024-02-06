0c71a5e5f58e6ede4410f16eb96f96d59e124991
Amir <amir.smagul01@gmail.com>
2024-02-07 02:58:55 +0600

    first

---
+++ app/src/main/java/com/example/homework/MainActivity.kt
@@ -0,0 +1,51 @@
+package com.example.homework
+
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        /// binding = ActivityMainBinding.inflate(getLayoutInflater());
+        /// setContentView(binding.getRoot());
+        Log.d(TAG, "onCreate")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(TAG, "onRestart")
+    }
+
+    companion object {
+        private const val TAG = "MainActivity"
+    }
+}
